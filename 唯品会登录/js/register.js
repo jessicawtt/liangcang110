93994303d3ba1901e9010d35cb7b87880d181122
jessicawtt <1481270830@qq.com>
2017-10-30 17:41:37 +0800

    Add files via upload

---
+++ 唯品会登录/js/register.js
@@ -0,0 +1,10 @@
+
+    $(".cellphone1").blur(function(){
+        $(".shuru1").css("visibility","visible");
+    });
+    $(".cellphone2").blur(function(){
+        $(".shuru2").css("visibility","visible");
+    });
+    $(".genD").click(function(){
+        $(".slabel").css("display","block");
+    });
